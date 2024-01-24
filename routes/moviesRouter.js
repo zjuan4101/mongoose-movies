f193f600213060b8c45b9ee53bfe642c55b5edb0
Juan Zelaya Castillo <zjuan4101@gmail.com>
2024-01-23 19:41:56 -0500

    added movie routes

--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -2,4 +2,13 @@ const express = require('express')
 const router = express.Router()
 const movieCtrl = require('../controllers/moviesController')
 
+router.post('/', movieCtrl.create)
+router.get('/', movieCtrl.index)
+router.get('/:id', movieCtrl.show)
+router.get('/:movieId/performers/:performerId', movieCtrl.addPerformer)
+
+
+
+
+
 module.exports = router
